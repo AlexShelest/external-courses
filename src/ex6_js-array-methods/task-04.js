a61b70b21493254dabf044c2282b010d7e2f4b7a
AlexShelest <indizx@gmail.com>
2019-04-03 12:51:16 +0300

    Filter function implemented.

---
+++ src/ex6_js-array-methods/task-04.js
@@ -0,0 +1,13 @@
+function filter(arr, callback, thisArg) {
+  var i,
+    length = arr.length,
+    results = [];
+  for (i = 0; i < length; i = i + 1) {
+    if (callback.call(thisArg, arr[i], i, arr)) {
+      results.push(arr[i]);
+    }
+  }
+  return results;
+}
+
+module.exports = filter;
